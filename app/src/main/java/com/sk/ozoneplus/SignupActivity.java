a84fe5f14c1406e734056eef55ce7e5df0e30717
ShimaK <shimak2013@gmail.com>
2017-05-23 13:50:51 +0530

    Validation Added

--- app/src/main/java/com/sk/ozoneplus/SignupActivity.java
+++ app/src/main/java/com/sk/ozoneplus/SignupActivity.java
@@ -1,13 +1,235 @@
 package com.sk.ozoneplus;
 
+import android.app.DatePickerDialog;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.app.ProgressDialog;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SignupActivity extends AppCompatActivity {
 
+    private static final String TAG = "SignupActivity";
+    private IntegrateCloud cloud;
+
+    @BindView(R.id.f_name)
+    EditText _fnameText;
+    @BindView(R.id.l_name)
+    EditText _lnameText;
+    @BindView(R.id.username_txt)
+    EditText _usernameText;
+    @BindView(R.id.email)
+    EditText _emailText;
+    @BindView(R.id.input_password)
+    EditText _passwordText;
+    @BindView(R.id.btn_signup)
+    Button _signupButton;
+    @BindView(R.id.link_login)
+    TextView _loginLink;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+        _loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the registration screen and return to the Login activity
+                finish();
+            }
+        });
     }
+
+    @OnClick(R.id.signup)
+    public void signup() {
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        _signupButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this,
+                R.style.AppTheme_NoActionBar);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+
+        String fname = _fnameText.getText().toString();
+        String lname = _lnameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        // TODO: Implement your own signup logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onSignupSuccess or onSignupFailed
+                        // depending on success
+                        onSignupSuccess();
+                        // onSignupFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+    public void onSignupSuccess() {
+        _signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+        _signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String fname = _fnameText.getText().toString();
+        String lname = _lnameText.getText().toString();
+        String username = _usernameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (fname.isEmpty() || fname.length() < 3) {
+            _fnameText.setError("at least 3 characters");
+            valid = false;
+        } else {
+            _fnameText.setError(null);
+        }
+
+        if (lname.isEmpty() || lname.length() < 3) {
+            _lnameText.setError("at least 3 characters");
+            valid = false;
+        } else {
+            _lnameText.setError(null);
+        }
+
+        if (username.isEmpty() || username.length() < 3) {
+            _usernameText.setError("at least 3 characters");
+            valid = false;
+        } else {
+            _usernameText.setError(null);
+        }
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        return valid;
+    }
+
+    private class IntegrateCloud extends AsyncTask<String, Void, Boolean> {
+
+        private final String fname, lname, user_username, email, user_password;
+
+        private final static String connectionString =
+                "jdbc:jtds:sqlserver://appmaskdb.database.windows.net:1433;instance=SQLEXPRESS;DatabaseName=AppMaskDB;";
+        private final static String userAdmin = "maskAdmin@appmaskdb";
+        private final static String password = "Sdgp12345678";
+
+        // Declare the JDBC objects.
+        private Connection con = null;
+        private Statement stmt = null;
+        private ResultSet rs = null;
+
+        public IntegrateCloud(String fname, String lname, String user_username, String email, String user_password) {
+            this.fname = fname;
+            this.lname = lname;
+            this.user_username = user_username;
+            this.email = email;
+            this.user_password = user_password;
+        }
+
+        @Override
+        protected Boolean doInBackground(@NonNull String... params) {
+
+            try {
+                // Establish the connection.
+                Class.forName("net.sourceforge.jtds.jdbc.Driver");
+                con = DriverManager.getConnection(connectionString, userAdmin, password);
+
+                String SQL = "INSERT INTO [user] (userName, password, email, firstName, lastName, dob)" +
+                        "VALUES ('" + user_username + "','" + user_password + "','" + email + "','" + fname + "','" + lname + "','" + "DOB" + "')";
+
+                stmt = con.createStatement();
+                rs = stmt.executeQuery(SQL);
+
+                while (rs.next()) {
+                    if (rs.getString("password").equals(user_password)) {
+                        return true;
+                    }
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (rs != null) try {
+                    rs.close();
+                } catch (Exception ignored) {
+                }
+                if (stmt != null) try {
+                    stmt.close();
+                } catch (Exception ignored) {
+                }
+                if (con != null) try {
+                    con.close();
+                } catch (Exception ignored) {
+                }
+            }
+
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            super.onPostExecute(result);
+
+            /*loginTask = null;
+
+            if (result) {
+                next(username);
+            } else {
+                showError("Invalid username or password \nTry again");
+            }*/
+        }
+    }
+
 }
