f4429905a4570dd4fa88cd73d636a01b05d3de8b
ShimaK <shimak2013@gmail.com>
2017-05-23 22:45:11 +0530

    Progress dialog added

--- app/src/main/java/com/sk/ozoneplus/SignupActivity.java
+++ app/src/main/java/com/sk/ozoneplus/SignupActivity.java
@@ -1,8 +1,12 @@
 package com.sk.ozoneplus;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,6 +24,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Calendar;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,21 +35,17 @@ public class SignupActivity extends AppCompatActivity {
 
     private static final String TAG = "SignupActivity";
     private IntegrateCloud cloud;
+    //TODO modify progress dialog
+    private ProgressDialog progressDialog;
 
-    @BindView(R.id.f_name)
-    EditText _fnameText;
-    @BindView(R.id.l_name)
-    EditText _lnameText;
-    @BindView(R.id.username_txt)
-    EditText _usernameText;
-    @BindView(R.id.email)
-    EditText _emailText;
-    @BindView(R.id.input_password)
-    EditText _passwordText;
-    @BindView(R.id.btn_signup)
-    Button _signupButton;
-    @BindView(R.id.link_login)
-    TextView _loginLink;
+    @BindView(R.id.f_name) EditText _fnameText;
+    @BindView(R.id.l_name) EditText _lnameText;
+    @BindView(R.id.username_txt) EditText _usernameText;
+    @BindView(R.id.email) EditText _emailText;
+    @BindView(R.id.input_password) EditText _passwordText;
+    @BindView(R.id.btn_signup) Button _signupButton;
+    @BindView(R.id.date_picker) EditText _datePickerText;
+    @BindView(R.id.link_login) TextView _loginLink;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -58,9 +60,27 @@ public class SignupActivity extends AppCompatActivity {
                 finish();
             }
         });
+        addListenerOnButton();
     }
 
-    @OnClick(R.id.signup)
+    @OnClick(R.id.date_picker)
+    public void addListenerOnButton() {
+        final Calendar calendar = Calendar.getInstance();
+        int yy = calendar.get(Calendar.YEAR);
+        int mm = calendar.get(Calendar.MONTH);
+        int dd = calendar.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog datePicker = new DatePickerDialog(SignupActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                String date = String.valueOf(year) + "-" + String.valueOf(monthOfYear)
+                        + "-" + String.valueOf(dayOfMonth);
+                _datePickerText.setText(date);
+            }
+        }, yy, mm, dd);
+        datePicker.show();
+    }
+
+    @OnClick(R.id.btn_signup)
     public void signup() {
         Log.d(TAG, "Signup");
 
@@ -71,20 +91,24 @@ public class SignupActivity extends AppCompatActivity {
 
         _signupButton.setEnabled(false);
 
-        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this,
-                R.style.AppTheme_NoActionBar);
+        progressDialog = new ProgressDialog(SignupActivity.this, R.style.AppTheme_NoActionBar);
         progressDialog.setIndeterminate(true);
         progressDialog.setMessage("Creating Account...");
         progressDialog.show();
 
         String fname = _fnameText.getText().toString();
         String lname = _lnameText.getText().toString();
+        String username = _usernameText.getText().toString();
         String email = _emailText.getText().toString();
         String password = _passwordText.getText().toString();
+        String dob = _datePickerText.getText().toString();
+
+        cloud = new IntegrateCloud(fname, lname, username, email, password, dob);
+        cloud.execute();
 
         // TODO: Implement your own signup logic here.
 
-        new android.os.Handler().postDelayed(
+        /*new android.os.Handler().postDelayed(
                 new Runnable() {
                     public void run() {
                         // On complete call either onSignupSuccess or onSignupFailed
@@ -93,13 +117,13 @@ public class SignupActivity extends AppCompatActivity {
                         // onSignupFailed();
                         progressDialog.dismiss();
                     }
-                }, 3000);
+                }, 3000);*/
     }
 
     public void onSignupSuccess() {
         _signupButton.setEnabled(true);
         setResult(RESULT_OK, null);
-        finish();
+        //finish();
     }
 
     public void onSignupFailed() {
@@ -114,6 +138,7 @@ public class SignupActivity extends AppCompatActivity {
         String lname = _lnameText.getText().toString();
         String username = _usernameText.getText().toString();
         String email = _emailText.getText().toString();
+        String dob = _datePickerText.getText().toString();
         String password = _passwordText.getText().toString();
 
         if (fname.isEmpty() || fname.length() < 3) {
@@ -151,12 +176,19 @@ public class SignupActivity extends AppCompatActivity {
             _passwordText.setError(null);
         }
 
+        if (dob.isEmpty()) {
+            _datePickerText.setError("Date of birth is empty");
+            valid = false;
+        } else {
+            _datePickerText.setError(null);
+        }
+
         return valid;
     }
 
-    private class IntegrateCloud extends AsyncTask<String, Void, Boolean> {
+    private class IntegrateCloud extends AsyncTask<Void, Void, Boolean> {
 
-        private final String fname, lname, user_username, email, user_password;
+        private final String fname, lname, user_username, email, user_password, dob;
 
         private final static String connectionString =
                 "jdbc:jtds:sqlserver://appmaskdb.database.windows.net:1433;instance=SQLEXPRESS;DatabaseName=AppMaskDB;";
@@ -166,18 +198,18 @@ public class SignupActivity extends AppCompatActivity {
         // Declare the JDBC objects.
         private Connection con = null;
         private Statement stmt = null;
-        private ResultSet rs = null;
 
-        public IntegrateCloud(String fname, String lname, String user_username, String email, String user_password) {
+        IntegrateCloud(String fname, String lname, String user_username, String email, String user_password, String dob) {
             this.fname = fname;
             this.lname = lname;
             this.user_username = user_username;
             this.email = email;
             this.user_password = user_password;
+            this.dob = dob;
         }
 
         @Override
-        protected Boolean doInBackground(@NonNull String... params) {
+        protected Boolean doInBackground(Void... params) {
 
             try {
                 // Establish the connection.
@@ -185,26 +217,17 @@ public class SignupActivity extends AppCompatActivity {
                 con = DriverManager.getConnection(connectionString, userAdmin, password);
 
                 String SQL = "INSERT INTO [user] (userName, password, email, firstName, lastName, dob)" +
-                        "VALUES ('" + user_username + "','" + user_password + "','" + email + "','" + fname + "','" + lname + "','" + "DOB" + "')";
+                        "VALUES ('" + user_username + "','" + user_password + "','" + email + "','"
+                        + fname + "','" + lname + "','" + dob + "')";
 
                 stmt = con.createStatement();
-                rs = stmt.executeQuery(SQL);
-
-                while (rs.next()) {
-                    if (rs.getString("password").equals(user_password)) {
-                        return true;
-                    }
-                }
+                if (stmt.executeUpdate(SQL) == 1) return true;
 
             } catch (SQLException e) {
                 e.printStackTrace();
             } catch (Exception e) {
                 e.printStackTrace();
             } finally {
-                if (rs != null) try {
-                    rs.close();
-                } catch (Exception ignored) {
-                }
                 if (stmt != null) try {
                     stmt.close();
                 } catch (Exception ignored) {
@@ -221,14 +244,11 @@ public class SignupActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Boolean result) {
             super.onPostExecute(result);
-
-            /*loginTask = null;
-
-            if (result) {
-                next(username);
-            } else {
-                showError("Invalid username or password \nTry again");
-            }*/
+            if (result) onSignupSuccess();
+            else onSignupFailed();
+            progressDialog.dismiss();
+            cloud = null;
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
         }
     }
 
