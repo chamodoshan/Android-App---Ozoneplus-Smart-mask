4bf7d4b5513bd2dec46c6bdd41e51c1a64d8acb6
ShimaK <shimak2013@gmail.com>
2017-05-28 19:07:00 +0530

    initial commit

---
+++ app/src/test/java/com/sk/ozoneplus/LoginUnitTest.java
@@ -0,0 +1,12 @@
+package com.sk.ozoneplus;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Created by ShimaK on 27-May-17.
+ */
+@RunWith(JUnit4.class)
+public class LoginUnitTest {
+
+}
